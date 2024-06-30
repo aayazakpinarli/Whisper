ce881c0e58d728c2ce72d4ea6d8f43ce0c817f05
Ayşegül AKPINARLI <aysegul.akpinarli@ug.bilkent.edu.tr>
2024-06-30 22:24:03 +0300

    websocket AuthMiddlewareStack

--- MessageApp/asgi.py
+++ MessageApp/asgi.py
@@ -7,10 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.sessions import SessionMiddlewareStack
+from django.core.asgi import get_asgi_application
+from django.urls import path
+from myWhisper.consumers import WhisperConsumer
 import os
 
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MessageApp.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path("ws/chat/", WhisperConsumer.as_asgi()),
+            # Add more paths for other WebSocket consumers as needed
+        ])
+    ),
+})
+
