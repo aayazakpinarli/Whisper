205573b8b0ed144650cdc0f3f10957eae4afb550
Ayşegül AKPINARLI <aysegul.akpinarli@ug.bilkent.edu.tr>
2024-06-30 22:23:15 +0300

    channels installed & default settings done

--- MessageApp/settings.py
+++ MessageApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myWhisper',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = 'MessageApp.asgi.application'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MessageApp.settings')
