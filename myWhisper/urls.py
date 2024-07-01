9462d20131d8624f1274c0f1ae4d6263f4ac2f60
Ayşegül AKPINARLI <aysegul.akpinarli@ug.bilkent.edu.tr>
2024-07-01 16:49:03 +0300

    login logout added

--- myWhisper/urls.py
+++ myWhisper/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.myWhisper, name='myWhisper'),
     path('register/', views.register, name='register'),
-
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- myWhisper/views.py
+++ myWhisper/views.py
@@ -1,6 +1,9 @@
+from urllib import request
+
 from django.shortcuts import render, redirect
 from myWhisper.models import Users, Message
-from pyexpat.errors import messages
+from django.contrib import messages
+from django.contrib.auth.models import auth
 
 
 # Create your views here.
@@ -9,6 +12,30 @@ from pyexpat.errors import messages
 def myWhisper(request):
     return render(request, 'index.html')
 
+def login(request):
+
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+
 
 def register(request):
 
@@ -16,26 +43,28 @@ def register(request):
         password = request.POST['password1']
         confirm_password = request.POST['password2']
         if password != confirm_password:
-            messages.error(request, 'Passwords must match')
+            messages.info(request, 'Passwords must match!')
         else:
             email = request.POST['email']
             if Users.objects.filter(email=email).exists():
-                messages.error(request, 'Email exists')
+                messages.info(request, 'Email exists!')
             else:
                 username = request.POST['username']
                 if Users.objects.filter(username=username).exists():
-                    messages.error(request, 'Username exists')
+                    messages.info(request, 'Username exists!')
                 else:
                     first_name = request.POST['name']
                     last_name = request.POST['surname']
                     user = Users(name=first_name, last_name=last_name,
                                  email=email, username=username, password=password)
                     try:
-                        user.save()  # Save the user object to the database
+                        user.save()
                         print("User saved successfully!")
+                        return redirect('login')
                     except Exception as e:
                         print(f"Error saving user: {str(e)}")
-                    return redirect('/')
+                        messages.info(request, 'Save errorr!!')
+
+        return render(request, 'register.html')
     else:
         return render(request, 'register.html')
-
