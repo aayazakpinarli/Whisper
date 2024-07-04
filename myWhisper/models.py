d785ad48fa2d6b505916b8df1fbe8c25ef1b3bde
Ayşegül AKPINARLI <aysegul.akpinarli@ug.bilkent.edu.tr>
2024-07-04 08:58:02 +0300

    thread model added

--- myWhisper/models.py
+++ myWhisper/models.py
@@ -1,8 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import auth, User
+from django.contrib.auth import get_user_model
+from django.db.models import Q
 
 # Create your models here.
+User = get_user_model()
 
+class ThreadManager(models.Manager):
+    def by_user(self, **kwargs):
+        user = kwargs.get('user')
+        lookup = Q(first_person=user) | Q(second_person=user)
+        qs = self.get_queryset().filter(lookup).distinct()
+        return qs
+
+class Thread(models.Model):
+    first_person = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='thread_first_person')
+    second_person = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='thread_second_person')
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = ThreadManager()
+    class Meta:
+        unique_together = ['first_person', 'second_person']
+
+class ChatMessage(models.Model):
+    thread = models.ForeignKey(Thread, null=True, blank=True, related_name='chatmessage_thread', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 class Message(models.Model):
     id = models.AutoField(primary_key=True)
@@ -12,5 +37,3 @@ class Message(models.Model):
     type = models.CharField(max_length=50)
     content = models.TextField()
     is_read = models.BooleanField(default=False)
-
-
