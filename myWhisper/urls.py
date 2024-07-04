8589f192ee0920e8300e5c78578740ef2e4ccfe0
Ayşegül AKPINARLI <aysegul.akpinarli@ug.bilkent.edu.tr>
2024-07-04 08:59:06 +0300

    url views.messagePage = index

--- myWhisper/urls.py
+++ myWhisper/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from myWhisper import views as chat_views
 from . import views
 
 urlpatterns = [
-    path('', views.myWhisper, name='myWhisper'),
+    path("", views.messagesPage),
+    #path('', views.myWhisper, name='myWhisper'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
