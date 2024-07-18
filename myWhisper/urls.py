3c529dae9fb364ca38e64c2c2d20a2d3ff10afac
Ayşegül AKPINARLI <aysegul.akpinarli@ug.bilkent.edu.tr>
2024-07-18 13:12:06 +0300

    profile info added

--- myWhisper/urls.py
+++ myWhisper/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import MessagesPageView, LoginView, LogoutView, RegisterView
-
+from .views import MessagesPageView, LoginView, LogoutView, RegisterView, AddFriendView, ProfileView
 
 urlpatterns = [
     path('', MessagesPageView.as_view(), name='messagesPage'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('addFriend/', AddFriendView.as_view(), name='addFriend'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
