9408c429f84b293c3bb2544f53ce6d1be4298439
Ayşegül AKPINARLI <aysegul.akpinarli@ug.bilkent.edu.tr>
2024-07-18 13:12:50 +0300

    profile view done add friend is empty yet

--- myWhisper/views.py
+++ myWhisper/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.models import auth, User
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
@@ -10,13 +10,24 @@ from django.utils.decorators import method_decorator
 # Create your views here.
 
 
+# ensure that only authenticated users can access this page
+# dispatch: it will affect all HTTP methods like GET, POST
 @method_decorator(login_required, name='dispatch')
+# view inherited
 class MessagesPageView(View):
     def get(self, request):
+        # calls the custom manager method 'by_user', which retrieves all Thread instances
+        # where the logged-in user is either first_p or second_person
+        # prefetch: optimizing technique that fetches related chatMessage instances in a single query, instead of
+        # increasing num of hits to the db, in single query I can collect all the messages
+        # order_by (ORDER BY)
         threads = Thread.objects.by_user(user=request.user).prefetch_related('chatmessage_thread').order_by('timestamp')
+        # a context disc is created, includes threads query set
+        # this context will be passed to the template for rendering
         context = {
             'Threads': threads
         }
+
         return render(request, 'chat/messages.html', context)
 
 
@@ -83,3 +94,41 @@ class RegisterView(View):
                         print(f"Error saving user: {str(e)}")
                         messages.info(request, 'Error!!')
         return render(request, 'register.html')
+
+
+class AddFriendView(View):
+    def get(self, request):
+        return render(request, 'chat/addFriend.html')
+
+
+class ProfileView(View):
+    def get(self, request):
+        return render(request, 'chat/profile.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        old_password = request.POST.get('old_password')
+        new_password = request.POST.get('new_password')
+
+        user = request.user
+
+        if username:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username exists!')
+            else:
+                user.username = username
+                user.save()
+                return redirect('/')
+
+        if old_password:
+            if new_password:
+                if user.check_password(old_password):
+                    user.set_password(new_password)
+                    user.save()
+                    return redirect('/')
+                else:
+                    messages.info(request, 'Old password is incorrect!')
+            else:
+                messages.info(request, 'Please enter new password!')
+
+        return redirect('/profile')
