781bddfd2acc559266a67b03bacfcad7e9ec8ae8
Ayşegül AKPINARLI <aysegul.akpinarli@ug.bilkent.edu.tr>
2024-07-21 01:03:18 +0300

    verify view

--- myWhisper/views.py
+++ myWhisper/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from myWhisper.models import Thread
 from django.views import View
